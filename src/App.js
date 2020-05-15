ce105bf296953da9ee5abf022e70bfce4808075f
Tairone Livinalli <tairone.livinalli@softplan.com.br>
2020-05-14 21:55:12 -0300

    Create, Read, Delete implemented

--- src/App.js
+++ src/App.js
@@ -1,29 +1,106 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 
-import "./styles.css";
+import api from './services/api';
+
+import './styles.css';
 
 function App() {
-  async function handleAddRepository() {
-    // TODO
+  const [repositories, setRepositories] = useState([]);
+  const [title, setTitle] = useState('');
+  const [url, setUrl] = useState('');
+  const [techs, setTechs] = useState([]);
+
+  async function handleAddRepository(event) {
+    event.preventDefault();
+
+    try {
+      const repository = { title, url, techs };
+
+      const response = await api.post('repositories', repository);
+
+      setRepositories([...repositories, response.data]);
+
+      resetForm();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  function resetForm() {
+    setTitle('');
+    setUrl('');
+    setTechs([]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    try {
+      await api.delete(`repositories/${id}`);
+
+      const undeletedRepositories = repositories.filter(
+        (repository) => repository.id !== id
+      );
+
+      setRepositories(undeletedRepositories);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    async function getRepositories() {
+      const response = await api.get('repositories');
+
+      setRepositories(response.data);
+    }
+
+    getRepositories();
+  }, []);
+
+  function handleChangeTechs(text) {
+    const techs = text.split(',');
+    console.log(techs);
+    setTechs(techs);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.length > 0 ? (
+          repositories.map((repository) => (
+            <li key={repository.id}>
+              {repository.title}
+              <button onClick={() => handleRemoveRepository(repository.id)}>
+                Remover
+              </button>
+            </li>
+          ))
+        ) : (
+          <p>Você não possui nenhum repositório</p>
+        )}
       </ul>
 
-      <button onClick={handleAddRepository}>Adicionar</button>
+      <form onSubmit={handleAddRepository}>
+        <input
+          type="text"
+          placeholder="Title"
+          onChange={(event) => setTitle(event.target.value)}
+          required
+        />
+        <input
+          type="text"
+          placeholder="Url"
+          onChange={(event) => setUrl(event.target.value)}
+          required
+        />
+        <input
+          type="text"
+          placeholder="Techs"
+          onChange={(event) => handleChangeTechs(event.target.value)}
+          required
+        />
+
+        <button type="submit">Adicionar</button>
+      </form>
     </div>
   );
 }
